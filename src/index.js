e4dbefc454231da0364609e5266368bbeb5f2356
Bryan Vera <bveracachay@gmail.com>
2021-12-06 12:59:28 -0500

    cambiando comandos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 /* eslint-disable semi */
 const express = require('express');
 const app = express();
-const cors = require('cors');
 app.use(express.json());
-app.use(cors);
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
